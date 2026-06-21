25f51ca5bde3cd3f30f1d40bef5cb6d45bc4d767
hanasweex <sweexhana@gmail.com>
2026-06-21 17:13:26 +0900

    Fix Vercel crashing by moving Node out of root

--- local-server/server.js
+++ local-server/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the parent directory
+app.use(express.static(path.join(__dirname, '../')));
 
 // Fallback to index.html for any other requests
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(PORT, () => {
